441d6ea965e5f8aeec3f27a1556df0c9760a4e4e
kristenli <kyli2005@gmail.com>
2022-05-20 16:20:54 -0400

    commit tree ed65867b18f51d9b5e3aadd6cfefe507a7fdf7f5

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // interate from the beginning to the middle
+  // racecar 7 / 2 == 3.5
+  // bbbbbb 6 /3 == 3
+  for (let i = 0; i <word.length / 2; i++) {
+// for loop above will let me check to middle of the word, which works.
+   // check each letter to the corresponding letter from the end
+   const j = word.length -1 - i
+   // if any letters don't match, return false
+   if (word[i] !== word[j]) return false
+  }
+  // return true
+  return true;
 }
 
 /* 
   Add your pseudocode here
-*/
+  THAT MEANS if the 
+  first letter is the same as the last letter, and the second letter is the same 
+  as the second to last letter, etc. until we reach the middle, return true.
 
+  iterate from beginning to the middle
+      check each letter to the corresponding letter from the end
+         if any letters don't match, return false
+
+  return true
 /*
   Add written explanation of your solution here
 */
